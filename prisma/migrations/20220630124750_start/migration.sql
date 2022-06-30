2f5b9a8d190fa30d19796c41baab7d0c4182d3b2
Lincoln-Caique <lincolncaique@gmail.com>
2022-06-30 13:32:46 -0300

    12-change-deploy

--- prisma/migrations/20220630124750_start/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Log" (
-    "id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "temperature" TEXT NOT NULL,
-    "humidity" TEXT NOT NULL,
-
-    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
-);
