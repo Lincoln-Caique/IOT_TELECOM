8773195dcdf85c06e884810d66fe7e60ccefcfb3
Lincoln-Caique <lincolncaique@gmail.com>
2022-06-30 13:50:24 -0300

    13-change-deploy

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,9 @@ import { logController } from './useCases/CreateLogUseCase';
 
 const router = Router();
 
-router.post('/datalogger', (req,res) => {
-  return logController.handle(req,res);
-})
+// router.post('/datalogger', (req,res) => {
+//   return logController.handle(req,res);
+// })
 
 router.get('/', (req,res) => {
   return res.status(200).json({
